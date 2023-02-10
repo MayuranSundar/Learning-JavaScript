d88ce5c75f680a6edaf0fef87a36f524e63b7be8
MayuranSundar <61875478+MayuranSundar@users.noreply.github.com>
2023-02-10 12:00:57 +0000

    Finished calculator project

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,52 +1,41 @@
-let input = "";
-let inputs = [];
-let operator = "";
+// Intialising variables
+let equation = "" 
+const acBtn = document.querySelector(".ac")
+const numBtns = document.querySelectorAll(".num-button")
+const eqBtn = document.querySelector(".equal")
+const delBtn = document.querySelector(".del")
 
-function buttonDetector(id){
-    if(id === "AC"){
-        input = ""
-        inputs = []
-        document.querySelector("#result-box").textContent = input
+// All Clear (AC) button event listener
+acBtn.addEventListener("click", function(){
+    // Displaying 0 and clearing the equation
+    document.querySelector("#result-box").textContent = 0
+    equation = ""
+})
 
-    }else if(id === "+" ||id === "-" ||id === "*" || id === "/"){
-        if (input != ""){
-            inputs.push(input)
-        }
-        operator = id;
-        input = "";
-        console.log(inputs);
-        // 
-        document.querySelector("#result-box").textContent = input
+// All number buttons and operator buttons event listener
+numBtns.forEach(e => {
+    e.addEventListener("click", function(){
+        // Adding each input to the equation string and displayed 
+        equation += e.textContent
+        document.querySelector("#result-box").textContent = equation
+    })
+})
 
-    }else if (id === "="){
-        inputs.push(input)
-        let output = 0;
-        // Execute code based on operator
-        if(inputs.length>=2){
-            if (operator === "+"){
-                output = parseInt(inputs[0]) + parseInt(inputs[1])
-            }else if (operator === "-"){
-                output = parseInt(inputs[0]) - parseInt(inputs[1]);
-            }else if(operator === "*"){
-                output = parseInt(inputs[0]) * parseInt(inputs[1]);
-            } else if(operator === "/"){
-                output = parseInt(inputs[0]) / parseInt(inputs[1]);
-            }
-            inputs=[]
-            inputs.push(output)
-            console.log("output", output)
-
-            document.querySelector("#result-box").textContent = output;
-
-        }
-    } else{
-        input += id
-        //
-        document.querySelector("#result-box").textContent = input
-
+// Equal button's event listener
+eqBtn.addEventListener("click", function(){
+    // if equation is not empty, the equation is evaluated and displayed
+    if(equation){
+        let output = eval(equation)
+        equation = output
+        document.querySelector("#result-box").textContent = output
     }
-    console.log(id)
-    // document.querySelector("#result-box").textContent = input
-}
+})
 
-document.querySelector("#result-box").textContent = output
+// Delete button's event listener 
+delBtn.addEventListener("click", function(){
+    // if equation is not empty the last input is deleted and displayed
+    if(equation){
+        equation = equation.slice(0,-1)
+        document.querySelector("#result-box").textContent = equation
+    }
+})
